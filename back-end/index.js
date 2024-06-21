40b8de34c4e210ab51084ceeb203be656522db18
Patryk Wozniczka <patryk.wozniczka@gmail.com>
2024-06-21 15:05:14 -0400

    Restrectured the repo

--- back-end/index.js
+++ back-end/index.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname,'..', 'public')));
 
 app.use('/users', users);
 
